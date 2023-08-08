a5dd7d0c4001dde996fe2447faf28d0886c1b5ce
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-08-09 03:14:32 +0530

    Update Linux.py

--- RAM Dump/Linux.py
+++ RAM Dump/Linux.py
@@ -2,43 +2,45 @@ import datetime
 import tkinter as tk
 from tkinter import filedialog, ttk
 from tkinter import messagebox
-# from PIL import Image
-# from PIL import ImageTk
 import subprocess
+import getpass
+
 app = tk.Tk()
 app.geometry("900x400")
 app.title(string="Ram Dump Collection Tool")
+
 frame = tk.Frame(app)
 frame.pack(fill="both", expand=True)
-# img = Image.open("/home/ryx/Desktop/Kavach/KryptoniteUpdatedLogo.jpg")
-# img = img.resize((400, 150))
-# logo_image = ImageTk.PhotoImage(img)
-# logo_canvas = tk.Canvas(frame, width=400, height=200)
-# logo_canvas.grid(row=0, column=0, padx=10, pady=10)
-# logo_canvas.create_image(190, 78, image=logo_image)
-options = ["Windows","Linux"]
+
+options = ["Windows", "Linux"]
 selected_option = tk.StringVar()
-option_combobox = ttk.Combobox(frame, textvariable=selected_option, values=options, state="readonly")
+option_combobox = ttk.Combobox(frame, textvariable=selected_option, 
+                               values=options, state="readonly")
 option_combobox.grid(row=1, column=0, padx=10, pady=10)
+
 file_path_var = tk.StringVar()
+
 current_time = datetime.datetime.now().time()
 Time = current_time.strftime("%H:%M")
+
 def dumpIT():
-    command = f'sudo insmod ./lime-6.3.0-kali1-amd64.ko \"path=/{Time}.mem"'
-    sudo_password = "kali123"
-    completed_process = subprocess.run(
-        command,
-        shell=True,
-        input=sudo_password.encode('utf-8'),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    if completed_process.returncode == 0:
-        messagebox.showinfo("Successfull")
-    else:
-        messagebox.showerror("Error galat code")
+    module_name = "lime"
+    check_module_command = ["lsmod"]
+    completed_process = subprocess.run(check_module_command, capture_output=True, text=True)
+
+    if module_name in completed_process.stdout:
+        # Unload the module
+        unload_command = ["sudo", "rmmod", module_name]
+        subprocess.run(unload_command)
+
+# Load the module
+    load_command = ["sudo", "insmod", "./lime-5.16.0-12parrot1-amd64.ko", f"path=/{Time}.mem", "format=raw"]
+    subprocess.run(load_command)
+
 dump_button = tk.Button(frame, text="Dump", command=dumpIT)
 dump_button.grid(row=3, column=0, padx=10, pady=10)
+
 analyze_button = tk.Button(frame, text="Analyze")
 analyze_button.grid(row=4, column=0, padx=10, pady=10)
+
 app.mainloop()
